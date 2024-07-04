1d272d8d54614410c141f2d93cc1f3d71ffbacdc
YaoYan01 <yaoyanjiang1@gmail.com>
2024-07-04 08:59:00 -0400

    creating login page functionality and logout

--- fitness-tracker/src/components/Login.js
+++ fitness-tracker/src/components/Login.js
@@ -1,8 +1,45 @@
+import { useState } from "react"
+import { supabase } from "../supabaseClient"
+
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        try {
+            const { user, error } = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password,
+            })
+            if (error) throw error
+            console.log('User logged in:', user)
+            // Handle successful login (e.g., redirect to dashboard)
+        } catch (error) {
+            console.error('Error logging in:', error.message)
+            // Handle login error (e.g., show error message to user)
+        }
+    }
+
 
     return (
         <>
-        <p> YOU NEED TO LOG IN </p></>
+            <div>
+                <form onSubmit={handleLogin}>
+                    <input type='email' value={email} onChange={handleEmail} placeholder="Email" required></input>
+                    <input type='password' value={password} onChange={handlePassword} placeholder="Password" required></input>
+                    <button type='submit'>Login</button>
+                </form>
+            </div></>
     )
 }
 
--- fitness-tracker/src/components/Navbar.js
+++ fitness-tracker/src/components/Navbar.js
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
+import { supabase } from "../supabaseClient";
+
 
 const Navbar = () => {
+    const handleLogout = async () => {
+        try {
+            const { error } = await supabase.auth.signOut();
+            if (error) throw error;
+            // The session will be automatically set to null by the onAuthStateChange listener
+        } catch (error) {
+            console.error('Error logging out:', error.message);
+        }
+
+    };
+
     return (
         <div className='bg-gray-300 flex justify-between'>
             <Link to='/'>Fitness Go</Link>
-            <div className='flex justify-items-end'>
+            <div className='flex justify-center'>
                 <Link to='/meal-log'>Meal Log</Link>
+                <button onClick={handleLogout}>Logout</button>
             </div>
         </div>
     );
